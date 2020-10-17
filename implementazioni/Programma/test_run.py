ef32a2d5302746505936d5827be2280059612fde
andygaspar <andygaspar@libero.it>
2020-10-17 10:54:14 +0200

    first tidying up

---
+++ implementazioni/Programma/test_run.py
@@ -0,0 +1,14 @@
+from data import dfMaker
+import pandas as pd
+from Programma.Mip import mipModel
+from Programma.Amal import amal
+from Programma.UDPP import udppModel
+
+# df = pd.read_csv("../data/data_ruiz.csv")
+
+df_UDPP = dfMaker.df_maker(30, 5, distribution="uniform")
+
+udpp_model = udppModel.UDPPModel(df_UDPP)
+
+model = mipModel.MipModel(udpp_model.get_new_df())
+model.run()
